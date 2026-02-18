f6223bf2ffab2b85afa42b9c2e78a4fc541a39dd
indrajith <iindrajithvijayan@gmail.com>
2026-02-18 16:27:54 +0530

    oops and linq

--- basics/Class1.cs
+++ basics/Class1.cs
@@ -296,62 +296,62 @@ using System.Text;
 //        var orders = GetOrders();
 
 
-        //Console.WriteLine("1.group by city count");
+//Console.WriteLine("1.group by city count");
 
-        //var q1 = People.GroupBy(p => p.City)
-        //    .Select(g => new { city = g.Key, count = g.Count() });  // q1
+//var q1 = People.GroupBy(p => p.City)
+//    .Select(g => new { city = g.Key, count = g.Count() });  // q1
 
-        //foreach (var x in q1)
-        //    Console.WriteLine($"{x.city} - {x.count}");
-
-
-        //Console.WriteLine("top 3 oldest");
-        //var q2 = People.OrderByDescending(p => p.Age).Take(3);   // q2
-        //foreach (var p in q2)
-        //    Console.WriteLine($"{p.Name} {p.Age}");
-
-        //Console.WriteLine("Nmae starts with A and age <30");
-        //var q3 = People.Where(n => n.Age < 30 && n.Name.StartsWith("A"));    // q3
-        //foreach (var n in q3)
-        //    Console.WriteLine($"{n.Name}-{n.Age}");
-
-        //Console.WriteLine(" group by age range");
-        //var q4 = People.GroupBy(p =>
-        //p.Age <= 20 ? "teen" :
-        //p.Age <= 40 ? "adult" : "senior")
-        //    .Select(g => new { g.Key, Count = g.Count() });  // q4
-
-        //foreach (var n in q4)
-        //    Console.WriteLine($"{n.Count}-{n.Key}");
-
-        //Console.WriteLine("join people + orders");
-        //var q5 = People.Join(orders,
-        //    p => p.PersonId,
-        //    o => o.PersonId,
-        //    (p, o) => new { p.Name, o.Amount });   // q5r
-
-        //foreach (var x in q5)
-        ////    Console.WriteLine($"{x.Name}");
-
-        //Console.WriteLine("duplicate names");
-
-        //var q7 = People.GroupBy(g => g.Name)
-        //    .Where(g => g.Count() > 1)
-        //    .Select(g => g.Key);
-        //foreach (var name in q7)      // q7
-        //    Console.WriteLine(name);
+//foreach (var x in q1)
+//    Console.WriteLine($"{x.city} - {x.count}");
 
 
-        //Console.WriteLine("average age per city");
-        //var q8 = People.GroupBy(g => g.City)
-        //    .Select(g => new { g.Key, avg = g.Average(p => p.Age) });
+//Console.WriteLine("top 3 oldest");
+//var q2 = People.OrderByDescending(p => p.Age).Take(3);   // q2
+//foreach (var p in q2)
+//    Console.WriteLine($"{p.Name} {p.Age}");
 
-        //Console.WriteLine("dictionary name age");
-        //var q10 = People.GroupBy(p => p.Name)
-        //         .ToDictionary(g => g.Key, g => g.First().Age);
+//Console.WriteLine("Nmae starts with A and age <30");
+//var q3 = People.Where(n => n.Age < 30 && n.Name.StartsWith("A"));    // q3
+//foreach (var n in q3)
+//    Console.WriteLine($"{n.Name}-{n.Age}");
 
-        //foreach (var x in q10)
-        //    Console.WriteLine($"{x.Key}-{x.Value}");
+//Console.WriteLine(" group by age range");
+//var q4 = People.GroupBy(p =>
+//p.Age <= 20 ? "teen" :
+//p.Age <= 40 ? "adult" : "senior")
+//    .Select(g => new { g.Key, Count = g.Count() });  // q4
+
+//foreach (var n in q4)
+//    Console.WriteLine($"{n.Count}-{n.Key}");
+
+//Console.WriteLine("join people + orders");
+//var q5 = People.Join(orders,
+//    p => p.PersonId,
+//    o => o.PersonId,
+//    (p, o) => new { p.Name, o.Amount });   // q5r
+
+//foreach (var x in q5)
+////    Console.WriteLine($"{x.Name}");
+
+//Console.WriteLine("duplicate names");
+
+//var q7 = People.GroupBy(g => g.Name)
+//    .Where(g => g.Count() > 1)
+//    .Select(g => g.Key);
+//foreach (var name in q7)      // q7
+//    Console.WriteLine(name);
+
+
+//Console.WriteLine("average age per city");
+//var q8 = People.GroupBy(g => g.City)
+//    .Select(g => new { g.Key, avg = g.Average(p => p.Age) });
+
+//Console.WriteLine("dictionary name age");
+//var q10 = People.GroupBy(p => p.Name)
+//         .ToDictionary(g => g.Key, g => g.First().Age);
+
+//foreach (var x in q10)
+//    Console.WriteLine($"{x.Key}-{x.Value}");
 
 
 //    }
@@ -439,11 +439,11 @@ using System.Text;
 //        var sm = new StudentManager();
 //        var cm = new CourseManager();
 
-        
+
 //        cm.AddCourse(new Course { CourseId = 1, CourseName = "C#" });
 //        cm.AddCourse(new Course { CourseId = 2, CourseName = "Python" });
 
-      
+
 //        sm.AddStudent(new Student { Id = 1, Name = "Arun", Age = 25, CourseId = 1 });
 //        sm.AddStudent(new Student { Id = 2, Name = "Ajay", Age = 19, CourseId = 2 });
 //        sm.AddStudent(new Student { Id = 3, Name = "Anu", Age = 30, CourseId = 1 });
@@ -471,4 +471,244 @@ using System.Text;
 //        foreach (var x in studentsInCourse)
 //            Console.WriteLine(x.Name);
 //    }
+////}
+//interface IPayment
+//{
+//    void pay();
 //}
+//class upi:IPayment
+//{
+//    public void pay()
+//    {
+
+//        Console.WriteLine("upi payment");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+
+//        IPayment p1 = new upi();
+//abstract class Animal
+//const double V = 10.7;
+
+//{
+//    public void Eat()
+//    {
+//        Console.WriteLine("animal eating");
+//    }
+//    public abstract void sound();
+//}
+
+//class Dog : Animal
+//{
+//    public override void sound()
+//    {
+//        Console.WriteLine("dog is barking");
+//    }
+//}
+//class Program
+//{
+//   public  static void main()
+//    {
+//        Animal a = new Dog();
+//            a.Eat();
+//        a.sound();
+
+//    }
+//}
+
+//double a = 10.7;
+//int b = (int)a;
+//Console.WriteLine(b);
+
+//string s = "123";
+//int x = Convert.ToInt32(s);
+
+
+//string s = "100";
+//int x;
+//bool ok = int.TryParse(s,out x);
+
+//Console.WriteLine(ok);
+//Console.WriteLine(x);
+
+//void add ( ref int n)
+//{
+//    n = n + 5;
+//    Console.WriteLine(n);
+//}
+//int num = 25;
+//add( ref num);
+//Console.WriteLine(num);
+
+//void SetValue(out int x)
+//{
+//    x = 50;
+//    Console.WriteLine(x);
+//}
+
+//int a;
+//SetValue(out a);
+//Console.WriteLine(a);   // 50
+
+//class student
+//{
+
+//    public int id;
+//    public string name;
+//    public int age;
+//    public int marks;
+//}
+
+//class Program {
+
+//    public static void Main()
+//    {
+//        List<student> students = new List<student>()
+//        {
+//            new student { id = 1, name = "Arun", age = 20, marks = 80 },
+//            new student { id = 2, name = "Ajay", age = 22, marks = 40 },
+//            new student { id = 3, name = "Anu", age = 19, marks = 90 },
+//            new student { id = 4, name = "Vijay", age = 23, marks = 60 },
+//        };
+
+//        var highmarks = students.Where(p => p.marks > 70);
+//        foreach (var x in highmarks)
+//            Console.WriteLine(x.marks);
+
+
+//    }
+
+//}
+
+
+
+
+
+//}
+
+//class Programs
+//{
+//    static void Main()
+//    {
+//        List<Employee> employee = new List<Employee>
+//        {
+
+//           new Employee {Id = 1,Name = "mob",Salary= 10001, Age = 23, Department = "IT"},
+//           new Employee {Id = 2,Name = "tob",Salary= 30001, Age = 44, Department = "HR"},
+//           new Employee {Id = 3,Name = "kob",Salary= 15000, Age = 25, Department = "Finance"},
+//           new Employee {Id = 1,Name = "rob",Salary= 40001, Age = 45, Department = "IT"},
+////           new Employee {Id = 1,Name = "cob",Salary= 20001,Age = 35, Department = "Finance"}
+////       };class Employee
+//{
+//    public int Id;
+//    public string Name;
+//    public int Salary;
+//    public string Department;
+//    public int Age;
+
+
+//        //display all employees
+//        var allemployee = employee;
+
+
+//        foreach (var emp in allemployee)
+//        {
+//            Console.WriteLine($"{emp.Id}-{emp.Name}-{emp.Department}-{emp.Salary}-{emp.Age}");
+//        }
+
+//        //display age above 30
+//        var ageabove = employee.Where(x => x.Age > 30);
+
+//        foreach (var emp in ageabove)
+//        {
+//            Console.WriteLine($"{emp.Id}-{emp.Name}-{emp.Age}");
+//        }
+
+//        // display descenting order of salary
+
+//        var descentingsalary = employee.OrderByDescending(x => x.Salary);
+//        foreach (var emp in descentingsalary)
+//        {
+//            Console.WriteLine($"{emp.Name}-{emp.Salary}");
+//        }
+
+//        //5.Display Grouped Department
+//        var groupedDepartment = employee.GroupBy(e => e.Department);
+
+
+//        foreach (var group in groupedDepartment)
+//        {
+//            Console.WriteLine($"{group.Key}");
+
+//            foreach (var em in group)
+
+//                Console.WriteLine($"Name:{em.Name} - Department:{em.Department}");
+
+//        }
+
+//        //heighestsalary
+//        var highestSalary = employee.OrderByDescending(e => e.Salary).First();
+
+//        Console.WriteLine($"Name :{highestSalary.Name} Salary :{highestSalary.Salary}");
+
+
+//        //lowestsalary
+//        var lowestsalary = employee.OrderBy(e => e.Salary).First();
+//        Console.WriteLine($"Name:{lowestsalary.Name} Salary:{lowestsalary.Salary}");
+
+//        //second heighest salary
+//        var secondhighestsalary = employee.OrderByDescending(e => e.Salary).Skip(1).First();
+//        Console.WriteLine($"Name : {secondhighestsalary.Name} second heighest salary{secondhighestsalary.Salary} ");
+
+//        //find count
+//        var count = employee.Count(e => e.Department == "IT");
+//        {
+//            Console.WriteLine($"Count in IT = {count}");
+//        }
+
+//        //Take first Three Employees
+
+//        var take = employee.Take(3);
+//        foreach (var em in take)
+//        {
+//            Console.WriteLine($"{em.Name}:{em.Salary}");
+
+//        }
+//        // Correct way to sum salaries with even values
+//        var totalEvenSalary = employee.Where(emp => emp.Salary % 2 == 0).Sum(emp => emp.Salary);
+//        Console.WriteLine("Total Even Salary: " + totalEvenSalary);
+
+//var allemployee = employee;
+
+//var allemploye = employee
+// .Where(g => g.Age > 30);
+//foreach (var x in allemploye)
+//        //    Console.WriteLine($"{x.Id}{x.Age}");
+//    }
+//}
+class Student
+{
+    public int id;
+    public int age;
+
+}
+
+class program{
+
+    public static void Main() {
+        List<Student> students = new List<Student>
+{
+    new Student{id=1,age=22},
+    new Student{id=2,age=24}
+};
+var student = students;
+        foreach (var x in student)
+            Console.WriteLine($"{x.id}-{x.age}");
+
+    }
+
+}
